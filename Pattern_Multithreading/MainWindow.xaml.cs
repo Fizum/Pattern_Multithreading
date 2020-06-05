082e0fe3177bb906c439952d26c28578446a245d
Filippo <f.izum02@gmail.com>
2020-06-05 22:30:55 +0200

    aggiornato

--- Pattern_Multithreading/MainWindow.xaml.cs
+++ Pattern_Multithreading/MainWindow.xaml.cs
@@ -28,18 +28,16 @@ namespace Pattern_Multithreading
 		}
 
 		CancellationTokenSource ct;
-        Semaphore sem = new Semaphore(1, 2);
+        
 
 		private async void Btn_avvia_Click(object sender, RoutedEventArgs e)
 		{
 			ct = new CancellationTokenSource();
-            //worker_async wrk = new worker_async(ct, 10, 1000);
+            Semaphore sem = new Semaphore(1, 2);
             IProgress<int> progress = new Progress<int>(UpdateUI);
-            WorkerProgressAsync wrk = new WorkerProgressAsync(sem, ct, 100, 2000, progress);
-			
-			//IProgress<int> progress = new Progress<int>(UpdateUI);
-			//Workerprogress wrk = new Workerprogress(ct, 10, 1000, progress);
+            WorkerProgressAsync wrk = new WorkerProgressAsync(sem, ct, 10, 1000, progress);
 			await wrk.start();
+
 			MessageBox.Show("Mi dimentico del thread secondario e non attendo il thread secondario per visualizzare questo messaggio");
 		}
 
